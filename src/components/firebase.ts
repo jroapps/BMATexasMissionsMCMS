0879eb06daf52186868e8b6cb2c86ff84aefb774
jroapps <74680468+jroapps@users.noreply.github.com>
2021-12-16 14:29:06 -0600

    Update firebase.ts

--- src/components/firebase.ts
+++ src/components/firebase.ts
@@ -5,16 +5,18 @@ import 'firebase/firestore';
 import 'firebase/storage';
 import 'firebase/performance';
 
-// Firebase configuration
+/* Firebase configuration
+   Enter your own configuration details after creating a Firebase project
+*/
 const firebaseConfig = {
-    apiKey: "AIzaSyBz6fLHNftOXGJXGLshODEUbRKzB2_dsCc",
-    authDomain: "bma-texas-missions-mcms.firebaseapp.com",
-    databaseURL: "https://bma-texas-missions-mcms-default-rtdb.firebaseio.com",
-    projectId: "bma-texas-missions-mcms",
-    storageBucket: "bma-texas-missions-mcms.appspot.com",
-    messagingSenderId: "568320621902",
-    appId: "1:568320621902:web:391ebe53e2bcc52eba721f",
-    measurementId: "G-RQT1DNRDWT"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 
 // Firebase initialization
